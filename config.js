e0075f0bb307f3ef244b41dcf61949d123c8fbae
long.dang <84171134+longdangdjv@users.noreply.github.com>
2023-08-24 21:29:24 +0700

    remove

--- config.js
+++ config.js
@@ -1,14 +1,12 @@
-require('dotenv').config();
-
 // console.log(process.env.MYSQL_HOST);
 
 const config = {
   db: {
-    host: process.env.MYSQL_HOST || 'localhost',
-    port: process.env.MYSQL_PORT || '3306',
-    user: process.env.MYSQL_USER_NAME || 'root', /* MySQL User */
-    password: process.env.MYSQL_PASSWORD || 'root123', /* MySQL Password */
-    database: process.env.MYSQL_DB_NAME || 'db_zenblog' /* MySQL Database */
+    host: 'localhost',
+    port: '3306',
+    user: 'root', /* MySQL User */
+    password: 'root123', /* MySQL Password */
+    database: 'db_zenblog' /* MySQL Database */
   },
   listPerPage: 10,
 };
